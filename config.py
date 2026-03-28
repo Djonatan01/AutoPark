361388c842ba8e7ee6f2d8a718cf21e10d57aa9f
Diego Jonatan de Miranda <diegojonatanmiranda@gmail.com>
2026-03-28 16:20:02 -0300

    adiciona função admin_required para controle de acesso e melhora a formatação do código

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from flask import flash, redirect, url_for
+from flask_login import current_user
+from functools import wraps
 from dotenv import load_dotenv
 import os
 from flask import Flask
@@ -15,14 +18,28 @@ app = Flask(__name__,
 load_dotenv()
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.login_view = 'router.login.login'
 login_manager.login_message = 'Realize o login para acessar essa página!'
 login_manager.init_app(app)
+
+
+def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return login_manager.unauthorized()
+        if str(current_user.status).upper() != 'ADMIN':
+            flash('Acesso negado: administrador exigido.', 'error')
+            return redirect(url_for('router.home.index'))
+        return func(*args, **kwargs)
+
+    return wrapper
